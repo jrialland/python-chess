807132cb8bea25c3f3c6c137889dc2f737834911
Julien <julien.rialland@gmail.com>
2015-09-25 19:40:33 +0200

    do not redownload each time

--- build_win32.sh
+++ build_win32.sh
@@ -28,92 +28,72 @@ echo 'Project name : ' $prj_name
 
 export DEBIAN_FRONTEND=noninteractive
 
-#jump into a fresh 'build_win32 directory'
-rm -rf _win32 2>/dev/null
-mkdir _win32
+mkdir -p _win32
 pushd _win32
-
-function hr {
-	echo '----------'
-}
-
-hr
-#verify some dependencies
-function verify_pkg {
-	log_daemon_msg "verifying installation for package $1"
-	dpkg -l $1 > /dev/null
-    if [ $? != 0 ]; then
-        echo $1 'is missing' >&2
-        exit 255
-    fi
-    log_end_msg 0
-}
 
 pip install wheel --upgrade 2>&1 > /dev/null ||true
 
-hr
 #get pyinstaller
-echo "Fetching pyinstaller from github"
-git clone https://github.com/pyinstaller/pyinstaller
+if [ ! -d pyinstaller ]; then
+  echo "Fetching pyinstaller from github"
+  git clone https://github.com/pyinstaller/pyinstaller
+fi
 
-
-hr
 #get virtual-wine
-echo "Fetching 'virtual-wine' from github"
-git clone https://github.com/htgoebel/virtual-wine
+if [ ! -d virtual-wine ]; then
+  echo "Fetching 'virtual-wine' from github"
+  git clone https://github.com/htgoebel/virtual-wine
+fi
 
-hr
 #modify it a bit (remove winecfg at end of the script)
-echo "Patching 'virtual-wine'"
 cat virtual-wine/vwine-setup | grep -v /bin/winecfg > virtual-wine/vwine-setup.2
 rm virtual-wine/vwine-setup
 mv virtual-wine/vwine-setup.2 virtual-wine/vwine-setup
 chmod +x virtual-wine/vwine-setup
 
 #ACTIVATING "windows" virtualenv
-./virtual-wine/vwine-setup venv_wine
+if [ ! -d venv_wine ]; then 
+  ./virtual-wine/vwine-setup venv_wine
+fi
 source ./venv_wine/bin/activate
 
-hr
 #install python 2.7
-echo 'Installing Python 2.7.8 for win32'
-wget https://www.python.org/ftp/python/2.7.8/python-2.7.8.msi
-wine msiexec /a python-2.7.8.msi /qn "TARGETDIR=c:/Python27"
+if [ ! -f python-2.7.8.msi ]; then
+  echo 'Installing Python 2.7.8 for win32'
+  wget https://www.python.org/ftp/python/2.7.8/python-2.7.8.msi
+  wine msiexec /a python-2.7.8.msi /qn "TARGETDIR=c:/Python27"
+fi
 
-hr
 #install setuptools
-echo 'Installing setuptools'
-wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py
-wine c:/Python27/python.exe ez_setup.py
+if [ ! -f ez_setup.py ]; then
+  echo 'Installing pywin32'
+  wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py
+  wine c:/Python27/python.exe ez_setup.py
+  wine c:/Python27/Scripts/easy_install.exe pip
+  wget http://vorboss.dl.sourceforge.net/project/pywin32/pywin32/Build%20219/pywin32-219.win32-py2.7.exe
+  python -m wheel convert pywin32-219.win32-py2.7.exe
+  wine c:/Python27/Scripts/pip.exe install $(find ./ -name "*.whl")
+  unzip -p pywin32-219.win32-py2.7.exe SCRIPTS/pywin32_postinstall.py > venv_wine/drive_c/pywin32_postinstall.py || true
+  wine c:/Python27/python.exe c:/pywin32_postinstall.py -install
+fi
 
-hr
-#download pywin232 (non-interactive installation)
-echo 'Installing pywin32'
-wget http://vorboss.dl.sourceforge.net/project/pywin32/pywin32/Build%20219/pywin32-219.win32-py2.7.exe
-python -m wheel convert pywin32-219.win32-py2.7.exe
-wine c:/Python27/Scripts/easy_install.exe pip
-wine c:/Python27/Scripts/pip.exe install $(find ./ -name "*.whl")
-
-echo 'Running pywin32 postinstall script'
-unzip -p pywin32-219.win32-py2.7.exe SCRIPTS/pywin32_postinstall.py > venv_wine/drive_c/pywin32_postinstall.py || true
-wine c:/Python27/python.exe c:/pywin32_postinstall.py -install
-
-
-hr
 #compile !
 echo "Compiling $target Into .exe"
 wine c:/Python27/python.exe pyinstaller/pyinstaller.py --onefile $target
-cp dist/$exe_name dist/$prj_name-noupx.exe
+mv dist/$exe_name dist/$prj_name-noupx.exe
+if [ ! -f upx391w.zip ]; then
+  echo 'Installing upx'
+  wget http://upx.sourceforge.net/download/upx391w.zip
+  unzip upx391w.zip
+fi
 
-wget http://upx.sourceforge.net/download/upx391w.zip
-unzip upx391w.zip
 wine upx391w/upx.exe dist/$exe_name
 
 #turn off virtual env
 deactivate
 
 if [ -d dist ]; then
-    mv dist $thisdir
+    mv -f dist $thisdir
     echo 'Generated executable in ' $thisdir/dist
 else
     echo 'Build Error'
